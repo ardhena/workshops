8f976df3be58f32597402c0e8169375c1b18f1cb
Olga <olgadraber@gmail.com>
2014-11-29 12:57:26 +0100

    fix specs for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Product do
   describe 'validations' do
+    it { should belong_to :user }
+    it { should belong_to :category }
+    it { should have_many :reviews }
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
     it { should validate_presence_of :price }
