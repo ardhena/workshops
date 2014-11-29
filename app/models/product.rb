394fecf947d31c60f3eb94cdcdb0e0711f23f73e
Olga <olgadraber@gmail.com>
2014-11-29 12:50:22 +0100

    add validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
 end
