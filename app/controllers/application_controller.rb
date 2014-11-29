669967f57a8005180c3a8d8525cba7e199c0b9e8
Olga <olgadraber@gmail.com>
2014-11-29 11:12:20 +0100

    add authenticate_admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,21 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname
-  end
-
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   protect_from_forgery with: :exception
+
+  private
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname
+  end
+
+  def authenticate_admin
+    if !current_user.admin?
+      redirect_to new_user_session_path
+    end
+	end
 end
