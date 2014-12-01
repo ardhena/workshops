526108931ab672b2940e9f3f3584ce27f77b94e7
Olga <olgadraber@gmail.com>
2014-12-01 23:34:14 +0100

    add default scope and validation to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :products
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+
+  default_scope { order('name ASC') }
 end
