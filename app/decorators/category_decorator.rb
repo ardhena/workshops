b60bbed5b795d4dac92726c19641a067f6a9c3e0
Olga <olgadraber@gmail.com>
2014-12-02 20:15:22 +0100

    format product owner and price

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -1,5 +1,6 @@
 class CategoryDecorator < Draper::Decorator
   delegate_all
+  decorates_association :products
 
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
