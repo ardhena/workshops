117479a992d13539b46d05112f555cc2edec3693
Olga <olgadraber@gmail.com>
2014-12-04 10:17:37 +0100

    add default scope to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,6 @@ class Review < ActiveRecord::Base
   validates :content, presence: true
   validates :rating, presence: true
   validates :user_id, presence: true
+
+  default_scope { order('created_at DESC') }
 end
